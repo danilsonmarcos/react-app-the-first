9f4175b1d2f3c8cf4e6ca3e64d4f7e0bc5a79747
danilsonmarcos <marcosdanilson0610@gmail.com>
2026-06-03 15:27:32 +0100

    Added 3 Buttons, to fix the concept about Passing Data between components

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,29 @@ function App() {
   return (
     <>
       <div>
-        <h1>Test title</h1>
-        <svg>
-          <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
-        </svg>
-        <form>
-          <input type="text" />
-        </form>
+        <Button text="Click Me" color="yellow" fontSize="20" />
+        <Button text="Don't Click Me!" color="pink" fontSize="40"/>
+        <Button text="Click Me" color="green" fontSize="30"/>
       </div>
-
     </>
   );
+}
+
+// a function that simbolize the button
+//adding props to make the changes more dynamic
+
+function Button(props){
+  //variable that will carry the styles to put in each button we create 
+  const buttonStyle = {
+    color: props.color, 
+    fontSize: props.fontSize + 'px'
+  }
+
+  return (
+    <>
+      <button style={buttonStyle}>{props.text}</button>
+    </>
+  ); 
 }
 
 export default App
